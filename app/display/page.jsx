eddf6066f99e3ddd5b1a447dcebf3349df38c1de
Pawanpathariya <pawanpathariys@gmail.com>
2025-04-10 09:48:30 +0530

    task3

--- app/display/page.jsx
+++ app/display/page.jsx
@@ -21,7 +21,7 @@ const router=useRouter();
       const data = response?.data?.data;
       setProduct(data);
       console.log(response.data.data);
-    
+       
     } catch (error) {
       console.error(error);
     }
